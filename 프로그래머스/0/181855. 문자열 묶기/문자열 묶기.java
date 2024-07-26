d65f14b616733d3381c3e57920f0bcdd2c39d37d
Jong Min <155048415+codejomo99@users.noreply.github.com>
2024-07-26 17:30:01 +0900

    [level 0] Title: 문자열 묶기, Time: 25.14 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(String[] strArr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(String str : strArr){
+            int length = str.length();
+            map.put(length, map.getOrDefault(length, 0) + 1);
+        }
+        
+        int max = 0;
+        
+        for(int count : map.values()){
+            if(count > max){
+                max = count;
+            }
+        }
+        
+        return max;
+    }
+}
