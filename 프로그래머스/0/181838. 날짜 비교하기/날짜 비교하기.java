4bbedb72dfbcda9404eb3bdd7ba5e2b1ec855251
Jong Min <155048415+codejomo99@users.noreply.github.com>
2024-08-02 17:47:25 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        int year1 = date1[0];
+        int year2 = date2[0];
+        int month1 = date1[1];
+        int month2 = date2[1];
+        int day1 = date1[2];
+        int day2 = date2[2];
+
+        if (year1 < year2) {
+            return 1;
+        } else if (year1 == year2) {
+            if (month1 < month2) {
+                return 1;
+            } else if (month1 == month2) {
+                if (day1 < day2) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+    }
+}
