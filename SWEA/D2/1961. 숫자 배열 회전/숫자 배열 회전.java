a9501de05ea197b9a39532b6a42a3f5c0730f986
Jong Min <155048415+codejomo99@users.noreply.github.com>
2024-09-24 17:20:14 +0900

    [D2] Title: 숫자 배열 회전, Time: 162 ms, Memory: 22,316 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -1,58 +1,74 @@
 import java.util.Scanner;
 
+class Solution {
 
-public class Solution {
+  public static void main(String args[]) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
 
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    for(int t = 1; t <= T; t++) {
+      int n = sc.nextInt();
+      int[][] arr = new int[n][n];
 
-    int T = scanner.nextInt(); 
-    for (int t = 1; t <= T; t++) {
-      int N = scanner.nextInt();  
-      int[][] matrix = new int[N][N];
-
-      
-      for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-          matrix[i][j] = scanner.nextInt();
+      for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+          arr[i][j] = sc.nextInt();
         }
       }
 
-      // 회전된 행렬 계산
-      int[][] rotated90 = rotate90(matrix, N);
-      int[][] rotated180 = rotate90(rotated90, N);
-      int[][] rotated270 = rotate90(rotated180, N);
+      // 90 도
+      int[][] arr1 = new int[n][n];
+      // 행
+      for(int i = 0; i < n; i++){
+        // 열
+        for(int j = 0; j < n; j++){
+          arr1[i][j] = arr[n-1-j][i];
+        }
+      }
 
-      // 출력 형식 맞추기
+      // 180 도
+      int[][] arr2 = new int[n][n];
+      // 행
+      for(int i = 0; i < n; i++){
+        // 열
+        for(int j = 0; j < n; j++){
+          arr2[i][j] = arr1[n-1-j][i];
+        }
+      }
+
+
+      // 270 도
+      int[][] arr3 = new int[n][n];
+      // 행
+      for(int i = 0; i < n; i++){
+        // 열
+        for(int j = 0; j < n; j++){
+          arr3[i][j] = arr2[n-1-j][i];
+        }
+      }
+
+
       System.out.println("#" + t);
+      for(int i = 0; i < n; i++){
 
-      for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-          System.out.print(rotated90[i][j]);
+        for(int j = 0; j < n; j++){
+          System.out.print(arr1[i][j]);
         }
         System.out.print(" ");
-        for (int j = 0; j < N; j++) {
-          System.out.print(rotated180[i][j]);
+        for(int j = 0; j < n; j++){
+          System.out.print(arr2[i][j]);
         }
         System.out.print(" ");
-        for (int j = 0; j < N; j++) {
-          System.out.print(rotated270[i][j]);
+        for(int j = 0; j < n; j++){
+          System.out.print(arr3[i][j]);
         }
         System.out.println();
       }
-    }
 
-    scanner.close();
+
+    }
+    sc.close();
   }
 
-  // 90도 회전 함수
-  private static int[][] rotate90(int[][] matrix, int N) {
-    int[][] result = new int[N][N];
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        result[j][N - 1 - i] = matrix[i][j];
-      }
-    }
-    return result;
-  }
+
 }
