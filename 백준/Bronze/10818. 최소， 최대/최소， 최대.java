2ca5284d785bc1a4d524d211fb15500c92cb2504
Jong Min <155048415+codejomo99@users.noreply.github.com>
2024-09-26 21:04:00 +0900

    [Bronze III] Title: 최소, 최대, Time: 440 ms, Memory: 91940 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,24 +1,34 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) throws IOException {
+    // BufferedReader 사용
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    int n = sc.nextInt();
+    // 첫 번째 입력: 배열의 크기
+    int n = Integer.parseInt(br.readLine());
     int[] arr = new int[n];
 
-    for(int i = 0; i <n; i++){
-      arr[i] = sc.nextInt();
+    // 입력값 한 줄로 받기 위해 StringTokenizer 사용
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
+
     int min = Integer.MAX_VALUE;
     int max = Integer.MIN_VALUE;
-    for(int i : arr){
-      min = Math.min(min,i);
-      max = Math.max(max,i);
+
+    for (int i : arr) {
+      min = Math.min(min, i);
+      max = Math.max(max, i);
     }
 
-
-    System.out.println(min+" "+max);
+    // 결과 출력
+    System.out.println(min + " " + max);
   }
 }
