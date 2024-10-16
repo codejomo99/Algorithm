ea221fdd4f8b05842e070420c9c826aa703ce095
Jong Min <155048415+codejomo99@users.noreply.github.com>
2024-10-16 16:31:45 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 168 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,49 +1,41 @@
-import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-  static int[] arr,ch;
-  static boolean flag =false;
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
 
-    arr = new int[9];
-    ch = new int[7];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    for(int i = 0; i < 9; i++){
-      arr[i] = sc.nextInt();
-    }
+        int[] arr = new int[9];
 
-    dfs(0,0,0);
-
-  }
-
-  public static void dfs(int idx, int L, int sum){
-
-    if(flag){
-      return;
-    }
-
-
-    if(L == 7){
-      if(sum == 100){
-        flag = true;
-        Arrays.sort(ch);
-        for(int x : ch){
-          System.out.println(x);
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            int n = sc.nextInt();
+            arr[i] = n;
+            sum += arr[i];
         }
-      }
 
-    }else{
+        Arrays.sort(arr);
 
-      if(idx >= 9) return;
+        int a = 0, b = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (sum - arr[j] - arr[i] == 100) {
+                    a = i;
+                    b = j;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i != a && i != b) {
+                System.out.println(arr[i]);
+            }
+        }
 
 
-      ch[L] = arr[idx]; // 현재 idx 의 난쟁이 선택
-      dfs(idx+1,L+1,sum+arr[idx]);
-      dfs(idx+1,L,sum);
     }
-  }
 }
