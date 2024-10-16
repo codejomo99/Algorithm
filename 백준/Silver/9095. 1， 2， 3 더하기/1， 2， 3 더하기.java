b65f3521e16b075fd36ff0602d41caa0b440788e
Jong Min <155048415+codejomo99@users.noreply.github.com>
2024-10-16 18:26:41 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 164 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        for (int t = 1; t <= T; t++) {
+            int n = sc.nextInt();
+
+            int[] dp = new int[11];
+
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+
+            for (int i = 4; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            }
+
+            System.out.println(dp[n]);
+
+        }
+
+
+    }
+}
