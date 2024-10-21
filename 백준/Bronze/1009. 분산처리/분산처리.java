1f67d860793ade46b3966c17fb0bfbb83ce1d4d0
Jong Min <155048415+codejomo99@users.noreply.github.com>
2024-10-21 13:44:54 +0900

    [Bronze II] Title: 분산처리, Time: 1056 ms, Memory: 21260 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.java
+++ 백준/Bronze/1009. 분산처리/분산처리.java
@@ -1,32 +1,26 @@
 import java.util.Scanner;
-import java.math.BigInteger;
 
-public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int T = sc.nextInt();
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    for (int t = 1; t <= T; t++) {
+        int T = sc.nextInt();
 
-      int n = sc.nextInt();
-      int m = sc.nextInt();
+        for (int t = 1; t <= T; t++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
 
+            int num = 1;
+            for (int i = 0; i < b; i++) {
+                num = (num * a) % 10;
+            }
 
-      int result = 1;
-      for(int i = 0; i < m; i++){
-        result = (result * n) % 10;
-      }
+            if (num == 0) {
+                System.out.println("10");
+            } else {
+                System.out.println(num);
+            }
+        }
 
-      if (result == 0) {
-        System.out.println("10");}
-      else{
-        System.out.println(result);
-      }
-
-   
     }
-
-
-    sc.close();
-  }
 }
