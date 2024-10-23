ea3d8fedf5fd39cff1c73b73c42e65841dd23ecc
Jong Min <155048415+codejomo99@users.noreply.github.com>
2024-10-23 16:32:45 +0900

    [Silver II] Title: 색종이 만들기, Time: 340 ms, Memory: 34428 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[][] board;
+    static int blue = 0;
+    static int white = 0;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        board = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        partition(0, 0, N);
+
+        System.out.println(white);
+        System.out.println(blue);
+    }
+
+    public static void partition(int row, int col, int size) {
+        if (colorCheck(row, col, size)) {
+            if (board[row][col] == 0) {
+                white++;
+            } else {
+                blue++;
+            }
+
+            return;
+        }
+
+        int newSize = size / 2;
+
+        partition(row, col, newSize); // 1사분면
+        partition(row, col + newSize, newSize); // 2사분면
+        partition(row + newSize, col, newSize); // 3사분면
+        partition(row + newSize, col + newSize, newSize); // 4사분면
+
+    }
+
+    public static boolean colorCheck(int row, int col, int size) {
+        int color = board[row][col];
+
+        for (int i = row; i < row + size; i++) {
+            for (int j = col; j < col + size; j++) {
+                if (board[i][j] != color) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
