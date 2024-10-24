667576113b4de733d1ef8123fcc61f440c90c2cc
Jong Min <155048415+codejomo99@users.noreply.github.com>
2024-10-24 16:19:48 +0900

    [Silver II] Title: 좌표 압축, Time: 2948 ms, Memory: 329260 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] origin = new int[N];
+        int[] sorted = new int[N];
+
+        HashMap<Integer, Integer> rankingMap = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            sorted[i] = origin[i] = sc.nextInt();
+        }
+
+        Arrays.sort(sorted);
+
+        int rank = 0;
+
+        for (int v : sorted) {
+            if (!rankingMap.containsKey(v)) {
+                rankingMap.put(v, rank);
+                rank++;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int key : origin) {
+            int ranking = rankingMap.get(key);
+            sb.append(ranking).append(' ');
+        }
+
+        System.out.println(sb);
+
+    }
+}
