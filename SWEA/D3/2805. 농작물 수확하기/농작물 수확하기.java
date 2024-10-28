90ff7f0fec24d2dd564ec5f5329a1a5b51426db1
Jong Min <155048415+codejomo99@users.noreply.github.com>
2024-10-28 16:31:55 +0900

    [D3] Title: 농작물 수확하기, Time: 167 ms, Memory: 25,220 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,40 +1,66 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Solution {
+class Solution {
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int T = sc.nextInt(); // 테스트 케이스 수
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
 
-    for (int t = 1; t <= T; t++) {
-      int n = sc.nextInt();
+        for (int t = 1; t <= T; t++) {
+            int n = sc.nextInt();
+            int mid = n / 2;
 
-      int[][] board = new int[n][n];
+            int[][] arr = new int[n][n];
 
-      for(int i = 0; i < n; i++){
-        String line = sc.next();
-        for(int j = 0; j < n; j++){
-          board[i][j] = line.charAt(j) - '0';
+            for (int i = 0; i < n; i++) {
+                String s = sc.next();
+                for (int j = 0; j < n; j++) {
+                    int num = s.charAt(j) - '0';
+
+                    arr[i][j] = num;
+                }
+            }
+
+            int result = 0;
+
+            // mid 기준으로 위로
+            int left = 0, right = 0;
+
+            for (int i = mid; i >= 0; i--) {
+
+                for (int j = left; j < n - right; j++) {
+                    result += arr[i][j];
+                }
+
+                left++;
+                right++;
+            }
+
+            // mid 기준으로 아래로
+            left = 0;
+            right = 0;
+
+            for (int i = mid; i < n; i++) {
+
+                for (int j = left; j < n - right; j++) {
+                    result += arr[i][j];
+                }
+
+                left++;
+                right++;
+            }
+
+            for (int i = 0; i < n; i++) {
+                result -= arr[mid][i];
+            }
+
+            System.out.println("#" + t + " " + result);
+
+
         }
-      }
 
-      int mid = (n/2);
-      int total = 0;
-
-      for(int i = 0; i < n; i++){
-        int start = Math.abs(mid-i);
-        int end = n-start;
-        for(int j = start; j < end; j++){
-          total += board[i][j];
-        }
-      }
-
-      System.out.printf("#%d %d\n",t,total);
 
     }
-  }
-
 
 
 }
-
