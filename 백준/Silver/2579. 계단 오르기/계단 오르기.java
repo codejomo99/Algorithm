e0d43050dd1823d1e2bafaa5f17cf7fade7563c8
Jong Min <155048415+codejomo99@users.noreply.github.com>
2024-11-02 18:33:48 +0900

    [Silver III] Title: 계단 오르기, Time: 180 ms, Memory: 17880 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,33 +1,32 @@
 import java.util.Scanner;
 
-public class Main {
-
+class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
 
-        int N = in.nextInt();
-
-        int[] DP = new int[N + 1];
-        int[] arr = new int[N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            arr[i] = in.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        // index = 0 은 시작점이므로 0이다.
-        DP[1] = arr[1];
+        int[] dp = new int[n + 1];
 
-        // N 이 1이 입력될 수도 있기 때문에 예외처리를 해줄 필요가 있다.
-        if (N >= 2) {
-            DP[2] = arr[1] + arr[2];
+        dp[1] = arr[0];
+        
+        if(n >= 2){
+            dp[2] = arr[0] + arr[1];
+        }
+        
+        
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 2] + arr[i - 1], dp[i - 3] + arr[i - 2] + arr[i - 1]);
         }
 
-        for (int i = 3; i <= N; i++) {
-            DP[i] = Math.max(DP[i - 2], DP[i - 3] + arr[i - 1]) + arr[i];
-        }
-
-        System.out.println(DP[N]);
+        System.out.println(dp[n]);
 
     }
+
 
 }
