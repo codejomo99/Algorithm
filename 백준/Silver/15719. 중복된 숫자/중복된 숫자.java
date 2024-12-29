3a00cd93ec452f9f807a55083da713ce394ca0ab
Jong Min <155048415+codejomo99@users.noreply.github.com>
2024-12-29 17:06:47 +0900

    [Silver V] Title: 중복된 숫자, Time: 1512 ms, Memory: 460980 KB -BaekjoonHub

--- 백준/Silver/15719. 중복된 숫자/중복된 숫자.java
+++ 백준/Silver/15719. 중복된 숫자/중복된 숫자.java
@@ -1,21 +1,23 @@
-import java.io.*;
-import java.util.*;
-public class Main{
-	public static void main(String[] args) throws IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		int n=Integer.parseInt(br.readLine());
-		boolean[] num=new boolean[n];
-		st=new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++) {
-			int a=Integer.parseInt(st.nextToken());
-			if(num[a]) {
-				bw.write(a+"");
-				break;
-			}
-				num[a]=true;
-		}
-		bw.flush();
-	}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        boolean[] num = new boolean[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            if (num[a]) {
+                System.out.println(a);
+                return;
+            }
+            num[a] = true;
+        }
+
+    }
 }
