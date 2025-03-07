b167cdf2344635f3c63d47d804f27912d1c0d3ce
Jong Min <155048415+codejomo99@users.noreply.github.com>
2025-03-07 12:57:48 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -3,31 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main {
+class Main{
 
-  static int N;
   static int[][] board;
   static boolean[] visit;
   static int answer = 0;
-
-
+  static int N;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     N = Integer.parseInt(br.readLine());
-    board = new int[N+1][N+1];
     visit = new boolean[N+1];
-
+    board = new int[N+1][N+1];
 
     int T = Integer.parseInt(br.readLine());
 
-    for (int i = 0; i < T; i++) {
+    for(int t = 0; t < T ; t++){
       StringTokenizer st = new StringTokenizer(br.readLine());
+
       int x = Integer.parseInt(st.nextToken());
       int y = Integer.parseInt(st.nextToken());
 
       board[x][y] = board[y][x] = 1;
     }
+
 
     dfs(1);
 
@@ -35,17 +34,15 @@ class Main {
 
   }
 
-  public static void dfs(int idx){
-    visit[idx] = true;
+  public static void dfs(int depth){
+    visit[depth] = true;
     answer++;
 
-
     for(int i = 1; i <= N; i++){
-      if(!visit[i] && board[idx][i] == 1){
+      if(!visit[i] && board[depth][i] == 1){
         dfs(i);
       }
     }
   }
-
 
 }
