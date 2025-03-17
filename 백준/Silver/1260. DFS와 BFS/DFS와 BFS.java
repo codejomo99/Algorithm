e3df04b32b4c5c96313478e1eb3164b2410648d6
Jong Min <155048415+codejomo99@users.noreply.github.com>
2025-03-17 10:32:56 +0900

    [Silver II] Title: DFS와 BFS, Time: 304 ms, Memory: 24684 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -7,70 +7,71 @@ import java.util.StringTokenizer;
 
 class Main{
 
-    static int n,m,v;
-    static int[][] board;
-    static boolean[] visit;
+  static int N, M, V;
+  static int[][] board, newBoard;
+  static boolean[] visit;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        v = Integer.parseInt(st.nextToken());
-
-        board = new int[n+1][n+1];
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    V = Integer.parseInt(st.nextToken());
 
 
+    board = new int[N+1][N+1];
 
-        for(int i = 0; i < m; i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+    for(int t = 0; t < M; t++){
+      st = new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(st.nextToken());
+      int y = Integer.parseInt(st.nextToken());
 
-            board[a][b] = board[b][a] = 1;
+      board[x][y] = board[y][x] = 1;
+    }
+
+    newBoard = board.clone();
+
+    visit = new boolean[N+1];
+    dfs(V);
+    System.out.println();
+    visit = new boolean[N+1];
+    bfs(V);
+  }
+
+  public static void dfs(int L){
+    System.out.print(L+" ");
+    visit[L] = true;
+
+    for(int i = 1; i <= N; i++){
+      if(board[L][i] == 1 && !visit[i]){
+        dfs(i);
+      }
+    }
+  }
+
+  public static void bfs(int L){
+    Queue<Integer> q = new LinkedList<>();
+    q.add(L);
+    visit[L] = true;
+
+    while(!q.isEmpty()){
+      int len = q.size();
+      for(int i = 0; i < len; i++){
+        int temp = q.poll();
+
+        System.out.print(temp + " ");
+
+        for(int j = 1; j <= N; j++){
+          if(newBoard[temp][j] == 1 && !visit[j]){
+            visit[j] = true;
+            q.add(j);
+          }
         }
-
-        visit = new boolean[n+1];
-        visit[v] = true;
-        DFS(v);
-        System.out.println();
-        visit = new boolean[n+1];
-        visit[v] = true;
-        BFS(v);
+      }
 
     }
 
-    public static void DFS(int L){
-        System.out.print(L+" ");
 
-        for(int i = 1; i <= n; i++){
-            if(board[L][i] == 1 && !visit[i]){
-                visit[i] = true;
-                DFS(i);
-            }
-        }
-    }
-
-    public static void BFS(int L){
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(L);
-
-        while(!q.isEmpty()){
-            int len = q.size();
-            for(int i = 0; i < len; i++){
-                int temp = q.poll();
-
-                System.out.print(temp + " ");
-
-                for(int j = 1; j <= n; j++){
-                    if(board[temp][j] == 1 && !visit[j]){
-                        visit[j] = true;
-                        q.add(j);
-                    }
-                }
-            }
-        }
-    }
+  }
 }
