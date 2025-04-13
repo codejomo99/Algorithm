9889404ce6bd6baffb4995305813d796bfd38c4f
Jong Min <155048415+codejomo99@users.noreply.github.com>
2025-04-13 17:50:13 +0900

    [Silver III] Title: N과 M (1), Time: 1628 ms, Memory: 63020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -4,41 +4,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Main{
+	static int N, M;
+	static int[] arr;
+	static boolean[] visit;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    static int N, M;
-    static int[] arr;
-    static boolean[] visit;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		arr = new int[M];
+		visit = new boolean[N];
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+		dfs(0,0);
 
-        arr = new int[M];
-        visit = new boolean[N];
+	}
 
-        dfs(0);
-    }
+	public static void dfs(int L, int S){
+		if(L == M){
+			for(int i : arr){
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}else{
+			for(int i = 0; i < N; i++){
+				if(!visit[i]){
+					visit[i] = true;
+					arr[L] = i+1;
+					dfs(L+1, S);
+					visit[i] = false;
+				}
+			}
+		}
+	}
 
-    public static void dfs(int depth){
-        if(depth == M){
-            for(int i : arr){
-                System.out.print(i +" ");
-            }
-            System.out.println();
-            return;
-        }else{
-            for(int i = 0; i < N; i++){
-                if(!visit[i]){
-                    visit[i] = true;
-                    arr[depth] = i+1;
-                    dfs(depth+1);
-                    visit[i] = false;
-                }
-            }
-        }
-    }
 }
